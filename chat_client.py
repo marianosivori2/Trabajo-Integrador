6d440fe295e6557bdf9296e814716ba6fbd7f65b
Nano <138333623+marianosivori2@users.noreply.github.com>
2024-06-04 11:00:29 -0300

    chat_client.py

---
+++ chat_client.py
@@ -0,0 +1,30 @@
+from twisted.internet import reactor, protocol
+from twisted.protocols.basic import LineReceiver
+
+class ChatClient(LineReceiver):
+    def connectionMade(self):
+        self.sendLine(b"Hola, servidor!")
+
+    def lineReceived(self, line):
+        print(f"Server: {line.decode('utf-8')}")
+
+class ChatClientFactory(protocol.ClientFactory):
+    def buildProtocol(self, addr):
+        return ChatClient()
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Error de Conexión.")
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print("Se perdió la Conexión.")
+        reactor.stop()
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python chat_client.py <host>")
+        sys.exit(1)
+    host = sys.argv[1]
+    reactor.connectTCP(host, 8000, ChatClientFactory())
+    reactor.run()
