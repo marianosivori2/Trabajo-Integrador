e1869532778335d3414b763bf7a1334ee46759f9
Nano <138333623+marianosivori2@users.noreply.github.com>
2024-06-11 19:29:21 -0300

    chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -5,18 +5,21 @@ import config
 class ChatClient(LineReceiver):
     def connectionMade(self):
         self.sendLine(b"Hola, Servidor!")
+        self.prompt_for_message()
 
     def lineReceived(self, line):
         print(f"Server: {line.decode('utf-8')}")
 
-        if line.strip() == b"Bienvenido al Chat del Servidor!":
-            self.prompt_for_message()
-
     def prompt_for_message(self):
-        message = input("Escribe tu mensaje: ")
-        self.sendLine(message.encode('utf-8'))
+        reactor.callInThread(self.get_input)
 
-
+    def get_input(self):
+        while True:
+            message = input("Escribe tu mensaje (o '/salir' para desconectarte): ")
+            if message == '/salir':
+                reactor.callFromThread(self.transport.loseConnection)
+                break
+            reactor.callFromThread(self.sendLine, message.encode('utf-8'))
 
 class ChatClientFactory(protocol.ClientFactory):
     def buildProtocol(self, addr):
@@ -38,3 +41,4 @@ if __name__ == "__main__":
     host = sys.argv[1]
     reactor.connectTCP(host, config.SERVER_PORT, ChatClientFactory())
     reactor.run()
+
