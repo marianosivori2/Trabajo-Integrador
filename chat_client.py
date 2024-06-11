77be37b15c77dd39ce526272df204d2db2800e30
Nano <138333623+marianosivori2@users.noreply.github.com>
2024-06-11 15:01:55 -0300

    chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -6,32 +6,35 @@ class ChatClient(LineReceiver):
     def connectionMade(self):
         self.sendLine(b"Hola, Servidor!")
 
-    def connectionLost(self, reason):
-        print("Se perdió la conexión con el servidor.")
-
     def lineReceived(self, line):
         print(f"Server: {line.decode('utf-8')}")
 
-        # Permitir enviar mensajes continuamente después de recibir el mensaje de bienvenida
-        self.prompt_for_messages()
+        if line.strip() == b"Bienvenido al Chat del Servidor!":
+            self.prompt_for_message()
 
-    def prompt_for_messages(self):
-        while True:
-            message = input("Escribe tu mensaje (o escribe 'quit' para salir): ")
-            if message.lower() == 'quit':
-                self.transport.loseConnection()  # Cierra la conexión
-                break
-            else:
-                self.sendLine(message.encode('utf-8'))
+    def prompt_for_message(self):
+        message = input("Escribe tu mensaje: ")
+        self.sendLine(message.encode('utf-8'))
+
+
 
 class ChatClientFactory(protocol.ClientFactory):
     def buildProtocol(self, addr):
         return ChatClient()
 
     def clientConnectionFailed(self, connector, reason):
-        print("Error de conexión.")
+        print("Error de Conexión")
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print("Se perdió la Conexión")
         reactor.stop()
 
 if __name__ == "__main__":
-    reactor.connectTCP(config.SERVER_HOST, config.SERVER_PORT, ChatClientFactory())
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python chat_client.py <host>")
+        sys.exit(1)
+    host = sys.argv[1]
+    reactor.connectTCP(host, config.SERVER_PORT, ChatClientFactory())
     reactor.run()
